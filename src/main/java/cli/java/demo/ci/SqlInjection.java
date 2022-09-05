724f243aef54260e71ac8a9dea0e6051f842c79a
psy <patrice.salathe@code-intelligence.com>
2022-09-05 10:52:37 +0200

    Add sqli example project & fuzz target

---
+++ src/main/java/cli/java/demo/ci/SqlInjection.java
@@ -0,0 +1,22 @@
+package cli.java.demo.ci;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.h2.jdbcx.JdbcDataSource;
+
+public class SqlInjection {
+    private Connection conn;
+
+    public void connect() throws SQLException {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2::resource:./database.db");
+        conn = ds.getConnection();
+        conn.createStatement().execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY PRIMARY KEY, username VARCHAR(50))");
+    }
+
+    public ResultSet getUserByUsername(String username) throws SQLException {
+        ResultSet result = conn.createStatement().executeQuery("SELECT * FROM users WHERE username='" + username + "'");
+        return result;
+    }
+}
---
+++ src/main/java/cli/java/demo/ci/SqlInjectionFuzzer.java
@@ -0,0 +1,18 @@
+package cli.java.demo.ci;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+
+import java.sql.SQLException;
+public class SqlInjectionFuzzer {
+
+    private static SqlInjection project;
+
+    public static void fuzzerInitialize() throws SQLException {
+        project = new SqlInjection();
+        project.connect();
+    }
+
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) throws SQLException {
+        project.getUserByUsername(data.consumeString(50));
+    }
+}
