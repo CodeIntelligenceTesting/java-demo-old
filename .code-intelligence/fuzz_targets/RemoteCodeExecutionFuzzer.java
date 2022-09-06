71458433cfc9fc551f29dfe8638351af0bf0bcb4
psy <patrice.salathe@code-intelligence.com>
2022-09-06 12:20:03 +0200

    Update RemoteCodeExecutionFuzzer.java

--- .code-intelligence/fuzz_targets/RemoteCodeExecutionFuzzer.java
+++ .code-intelligence/fuzz_targets/RemoteCodeExecutionFuzzer.java
@@ -1,12 +1,12 @@
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import cli.java.demo.ci.RemoteCodeExecution;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.lang.Exception;
 
 public class RemoteCodeExecutionFuzzer {
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
         try {
             RemoteCodeExecution.deserialize(new ByteArrayInputStream(data.consumeBytes(100)));
-        } catch (IOException ignored) {}
+        } catch (Exception ignored) {}
     }
 }
